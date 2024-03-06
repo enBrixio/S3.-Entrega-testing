525f2ee231df260a869cec70111e09da390f3640
Raul <raul.bricio@gmail.com>
2024-03-06 11:59:43 +0100

    Ejercicio 1

--- src/films.js
+++ src/films.js
@@ -1,8 +1,9 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  '';
-  console.log("EXERCICE 1 ->", result);
-  return result;
+  let result = array.map(movie => 
+   (movie.director));
+    console.log("EXERCICE 1 ->", result);
+    return result;
 }
 
 // Exercise 2: Get the films of a certain director
